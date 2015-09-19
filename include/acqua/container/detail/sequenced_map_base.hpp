39c92e2dabde0b9de2178803ed3aa4846d6bb116
Haruhiko Uchida <harre.orz@gmail.com>
2015-09-20 00:17:24 +0900

    sequenced_map をリファクタリング

--- include/acqua/container/detail/sequenced_map_base.hpp
+++ include/acqua/container/detail/sequenced_map_base.hpp
@@ -8,50 +8,55 @@
 
 #pragma once
 
-#include <algorithm>
-#include <functional>
-#include <boost/iterator_adaptors.hpp>
+#include <stdexcept>
 
 namespace acqua { namespace container { namespace detail {
 
-class unique_tag {};
-class non_unique_tag {};
-
-template <typename Tag, typename Key, typename Value, typename Pred, typename Allocator,
+template <typename Derived,
+          typename Key, typename Value, typename Pred, typename Alloc,
           template <typename T, typename A> class Container>
 class sequenced_map_base
-    : private Pred, private Allocator
+    : private Pred
+    , private Alloc
 {
+protected:
+    using base_type = sequenced_map_base<Derived, Key, Value, Pred, Alloc, Container>;
+
+    ~sequenced_map_base() {}
+
 public:
     using key_type = Key;
     using mapped_type = Value;
-    using value_type = std::pair<key_type const, mapped_type>;
+    using value_type = std::pair<Key const, Value>;
     using key_equal = Pred;
-    using allocator_type = Allocator;
+    using allocator_type = Alloc;
 
 private:
     struct wrapped_value_type : std::pair<key_type, mapped_type>
     {
         template <typename... Args>
-        wrapped_value_type(Args... args)
-            : std::pair<key_type, mapped_type>(args...) {}
+        wrapped_value_type(Args... args) : std::pair<key_type, mapped_type>(args...) {}
         operator value_type &() { return *reinterpret_cast<value_type *>(this); }
         operator value_type &() const { return *reinterpret_cast<value_type *>(const_cast<wrapped_value_type *>(this)); }
     };
-    using data_type = Container<wrapped_value_type, typename Allocator::template rebind<wrapped_value_type>::other>;
+    using data_type = Container<wrapped_value_type, typename Alloc::template rebind<wrapped_value_type>::other>;
 
 public:
-    using difference_type = typename std::iterator_traits<typename data_type::iterator>::difference_type;
-    using size_type = typename data_type::size_type;
+    explicit sequenced_map_base(key_equal const & key_eq, allocator_type const & alloc)
+        : key_equal(key_eq), allocator_type(alloc) {}
 
     struct iterator : boost::iterator_adaptor<iterator, typename data_type::iterator, value_type>
     {
+        iterator() {}
+
         iterator(typename data_type::iterator it)
             : boost::iterator_adaptor<iterator, typename data_type::iterator, value_type>(it) {}
     };
 
     struct const_iterator : boost::iterator_adaptor<const_iterator, typename data_type::const_iterator, value_type>
     {
+        const_iterator() {}
+
         const_iterator(typename data_type::const_iterator it)
             : boost::iterator_adaptor<const_iterator, typename data_type::const_iterator, value_type>(it) {}
 
@@ -61,199 +66,46 @@ public:
 
     struct reverse_iterator : boost::iterator_adaptor<reverse_iterator, typename data_type::reverse_iterator, value_type>
     {
+        reverse_iterator() {}
+
         reverse_iterator(typename data_type::reverse_iterator it)
             : boost::iterator_adaptor<reverse_iterator, typename data_type::reverse_iterator, value_type>(it) {}
     };
 
     struct const_reverse_iterator : boost::iterator_adaptor<const_reverse_iterator, typename data_type::const_reverse_iterator, value_type>
     {
-        const_reverse_iterator(iterator it)
-            : boost::iterator_adaptor<const_reverse_iterator, typename data_type::const_reverse_iterator, value_type>(it.base()) {}
+        const_reverse_iterator() {}
 
         const_reverse_iterator(typename data_type::const_reverse_iterator it)
             : boost::iterator_adaptor<const_reverse_iterator, typename data_type::const_reverse_iterator, value_type>(it) {}
+
+        const_reverse_iterator(reverse_iterator it)
+            : boost::iterator_adaptor<const_reverse_iterator, typename data_type::const_reverse_iterator, value_type>(it.base()) {}
     };
 
-    sequenced_map_base() = default;
+    using size_type = typename data_type::size_type;
+    using difference_type = typename std::iterator_traits<iterator>::difference_type;
 
-    explicit sequenced_map_base(key_equal pred, allocator_type alloc)
-        : data_(pred, alloc) {}
+public:
+    key_equal key_eq() const { return *static_cast<key_equal const *>(this); }
+    allocator_type get_allocator() const { return *static_cast<allocator_type const *>(this); }
+    bool empty() const { return data_.empty(); }
+    size_type size() const { return data_.size(); }
+    iterator begin() { return data_.begin(); }
+    const_iterator begin() const { return data_.begin(); }
+    iterator end() { return data_.end(); }
+    const_iterator end() const { return data_.end(); }
+    reverse_iterator rbegin() { return data_.rbegin(); }
+    const_reverse_iterator rbegin() const { return data_.rbegin(); }
+    reverse_iterator rend() { return data_.rend(); }
+    const_reverse_iterator rend() const { return data_.rend(); }
 
-    explicit sequenced_map_base(size_type size)
-        : data_(size) {}
-
-    explicit sequenced_map_base(const_iterator beg, const_iterator end)
-        : data_(std::distance(beg, end))
-    {
-        std::copy(beg.base(), end.base(), data_.begin());
-    }
-
-    template <typename Iterator>
-    explicit sequenced_map_base(Iterator beg, Iterator end)
-    {
-        // TODO
-    }
-
-    key_equal & key_eq()
-    { return *static_cast<key_equal *>(this); }
-
-    key_equal const & key_eq() const
-    { return *static_cast<key_equal const *>(this); }
-
-    allocator_type & get_allocator()
-    { return *static_cast<allocator_type *>(this); }
-
-    allocator_type const & get_allocator() const
-    { return *static_cast<allocator_type const *>(this); }
-
-    bool empty() const
-    { return data_.empty(); }
-
-    size_type size() const
-    { return data_.size(); }
-
-    void clear()
-    { data_.clear(); }
-
-    iterator begin()
-    { return data_.begin(); }
-
-    iterator end()
-    { return data_.end(); }
-
-    const_iterator begin() const
-    { return data_.begin(); }
-
-    const_iterator end() const
-    { return data_.end(); }
-
-    reverse_iterator rbegin()
-    { return data_.rbegin(); }
-
-    reverse_iterator rend()
-    { return data_.rend(); }
-
-    const_reverse_iterator rbegin() const
-    { return data_.rbegin(); }
-
-    const_reverse_iterator rend() const
-    { return data_.rend(); }
-
-    iterator find(key_type const & key)
-    { return std::find_if(begin(), end(), [this, &key](value_type const & e) { return key_eq()(e.first, key); }); }
-
-    const_iterator find(key_type const & key) const
-    { return std::find_if(begin(), end(), [this, &key](value_type const & e) { return key_eq()(e.first, key); }); }
-
-    template <typename Iterator>
-    Iterator to_next(unique_tag, Iterator it) const
-    {
-        if (it != end())
-            ++it;
-        return it;
-    }
-
-    template <typename Iterator>
-    Iterator to_next(non_unique_tag, Iterator it) const
-    {
-        if (it != end()) {
-            auto const & key = it->key;
-            do ++it; while(it != end() && key_eq(key, it->key));
-        }
-        return it;
-    }
-
-    size_type count(key_type const & key) const
-    {
-        auto it = find(key);
-        return to_next(Tag(), it) - it;
-    }
-
-    std::pair<iterator, iterator> equal_range(key_type const & key)
-    {
-        auto it = find(key);
-        return std::make_pair(it, to_next(Tag(), it));
-    }
-
-    std::pair<const_iterator, const_iterator> equal_range(key_type const & key) const
-    {
-        auto it = find(key);
-        return std::make_pair(it, to_next(Tag(), it));
-    }
-
-    std::pair<iterator, bool> insert(value_type const & val)
-    {
-        return insert_(Tag(), val);
-    }
-
-    iterator insert(const_iterator, value_type const & val)
-    {
-        return insert_(Tag(), val).first;
-    }
-
-    std::pair<iterator, bool> insert_(unique_tag, value_type const & val)
-    {
-        auto it = find(val.first);
-        if (it != end())
-            return std::make_pair(it, false);
-        data_.push_back(val);
-        return std::make_pair(--end(), true);
-    }
-
-    std::pair<iterator, bool> insert_(non_unique_tag, value_type const & val)
-    {
-        return std::make_pair(iterator(data_.insert(find(key(val.first)).base(), val)), true);
-    }
-
-    template <typename... Args>
-    std::pair<iterator, bool> emplace(Args... args)
-    {
-        return emplace_(Tag(), args...);
-    }
-
-    template <typename... Args>
-    std::pair<iterator, bool> emplace_(unique_tag, Args... args)
-    {
-        auto it = find(key(args...));
-        if (it != end())
-            return std::make_pair(it, false);
-        data_.emplace_back(args...);
-        return std::make_pair(--end(), true);
-    }
-
-    template <typename... Args>
-    std::pair<iterator, bool> emplace_(non_unique_tag, Args... args)
-    {
-        return std::make_pair(iterator(data_.emplace(find(key(args...)).base(), args...)), true);
-    }
-
-    template <typename... Args>
-    iterator emplace_hint(const_iterator, Args... args)
-    {
-        return emplace(args...).first;
-    }
-
-    iterator erase(const_iterator it)
-    {
-        return data_.erase(it.base());
-    }
-
-    iterator erase(const_iterator beg, const_iterator end)
-    {
-        return data_.erase(beg.base(), end.base());
-    }
-
-    iterator erase(key_type const & key)
-    {
-        auto it = find(key);
-        return data_.erase(it.base(), to_next(Tag(), it).base());
-    }
-
+    void clear() { data_.clear(); }
     mapped_type & at(key_type const & key)
     {
         auto it = find(key);
         if (it == end())
-            throw std::out_of_range("sequenced_map_base::at");
+            throw std::out_of_range("sequenced_map::at");
         return it->second;
     }
 
@@ -261,33 +113,45 @@ public:
     {
         auto it = find(key);
         if (it == end())
-            throw std::out_of_range("sequenced_map_base::at");
+            throw std::out_of_range("sequenced_map::at");
         return it->second;
     }
-
-    mapped_type & operator[](key_type const & key)
+    iterator find(key_type const & key)
     {
-        auto it = find(key);
-        if (it == end()) {
-            data_.emplace_back(key, mapped_type());
-            it = --end();
+        return std::find_if(data_.begin(), data_.end(), [&key](auto const & elem) { return key_equal()(elem.first, key); });
+    }
+    const_iterator find(key_type const & key) const
+    {
+        return std::find_if(data_.begin(), data_.end(), [&key](auto const & elem) { return key_equal()(elem.first, key); });
+    }
+    iterator erase(const_iterator it) { return data_.erase(it.base()); }
+    iterator erase(const_iterator beg, const_iterator end) { return data_.erase(beg.base(), end.base()); }
+
+    template <typename It>
+    iterator assign(It beg, It end)
+    {
+        clear();
+        return insert(beg, end);
+    }
+
+    template <typename It>
+    iterator insert(It beg, It end)
+    {
+        iterator it;
+        while(beg != end) {
+            it = static_cast<Derived *>(this)->insert(const_iterator(), *beg);
+            ++beg;
         }
-        return it->second;
+        return ++it;
     }
 
-    static key_type const & key(value_type const & val)
-    {
-        return val.first;
-    }
-
-    template <typename... Args>
-    static key_type const & key(key_type const & t, Args...)
-    {
-        return t;
-    }
-
-private:
+protected:
+    iterator find(value_type const & val) { return find(val.first); }
+    const_iterator find(value_type const & val) const { return find(val.first); }
+    iterator find(key_type const & key, mapped_type const &) { return find(key); }
+    const_iterator find(key_type const & key, mapped_type const &) const { return find(key); }
     data_type data_;
 };
+
 
 } } }
--- include/acqua/container/sequenced_map.hpp
+++ include/acqua/container/sequenced_map.hpp
@@ -10,6 +10,7 @@
 
 #include <deque>
 #include <list>
+#include <boost/operators.hpp>
 #include <acqua/container/detail/sequenced_map_base.hpp>
 
 namespace acqua { namespace container {
@@ -23,96 +24,188 @@ template <
     typename Key,
     typename Value,
     typename Pred = std::equal_to<Key>,
-    typename Allocator = std::allocator< std::pair<Key const, Value> >
+    typename Alloc = std::allocator< std::pair<Key const, Value> >
     >
 class sequenced_map
-    : detail::sequenced_map_base<detail::unique_tag, Key, Value, Pred, Allocator, std::deque>
+    : public acqua::container::detail::sequenced_map_base<sequenced_map<Key, Value, Pred, Alloc>, Key, Value, Pred, Alloc, std::deque>
+    , private boost::totally_ordered< sequenced_map<Key, Value, Pred, Alloc> >
 {
-    using base_type = detail::sequenced_map_base<detail::unique_tag, Key, Value, Pred, Allocator, std::deque>;
+private:
+    using base_type = typename sequenced_map::base_type;
+    using base_type::data_;
 
 public:
-    using key_type = typename base_type::key_type;
-    using mapped_type = typename base_type::mapped_type;
     using value_type = typename base_type::value_type;
-    using key_equal = typename base_type::key_equal;;
-    using allocator_type = typename base_type::allocator_type;
     using iterator = typename base_type::iterator;
     using const_iterator = typename base_type::const_iterator;
-    using reverse_iterator = typename base_type::reverse_iterator;
-    using const_reverse_iterator = typename base_type::const_reverse_iterator;
-    using difference_type = typename base_type::difference_type;
-    using size_type = typename base_type::size_type;
 
-    using base_type::base_type;
-    using base_type::key_eq;
-    using base_type::get_allocator;
-    using base_type::empty;
-    using base_type::size;
-    using base_type::clear;
     using base_type::begin;
     using base_type::end;
-    using base_type::rbegin;
-    using base_type::rend;
     using base_type::find;
-    using base_type::count;
-    using base_type::equal_range;
-    using base_type::insert;
-    using base_type::emplace;
-    using base_type::emplace_hint;
-    using base_type::erase;
-    using base_type::at;
-    using base_type::operator[];
+
+public:
+    sequenced_map(Pred const & key_eq = Pred(), Alloc const & alloc = Alloc())
+        : base_type(key_eq, alloc) {}
+
+    template <typename It>
+    explicit sequenced_map(It beg, It end)
+        : base_type(Pred(), Alloc())
+    {
+        base_type::insert(beg, end);
+    }
+
+    iterator insert(const_iterator, value_type const & val)
+    {
+        auto it = find(val.first);
+        if (it != end())
+            return it;
+        data_.push_back(val);
+        return --end();
+    }
+
+    std::pair<iterator, bool> insert(value_type const & val)
+    {
+        auto it = find(val.first);
+        if (it != end())
+            return std::make_pair(it, false);
+        data_.push_back(val);
+        return std::make_pair(--end(), true);
+    }
+
+    template <typename... Args>
+    iterator emplace_hint(const_iterator, Args... args)
+    {
+        auto it = find(args...);
+        if (it != end())
+            return it;
+        data_.emplace_back(args...);
+        return --end();
+    }
+
+    template <typename... Args>
+    std::pair<iterator, bool> emplace(Args... args)
+    {
+        auto it = find(args...);
+        if (it != end())
+            return std::make_pair(it, false);
+        data_.emplace_back(args...);
+        return std::make_pair(--end(), true);
+    }
+
+    Value & operator[](Key const & key)
+    {
+        auto it = base_type::find(key);
+        if (it == base_type::end()) {
+            data_.emplace_back(key, Value());
+            it = --base_type::end();
+        }
+        return it->second;
+    }
+
+    friend bool operator==(sequenced_map const & lhs, sequenced_map const & rhs)
+    {
+        return lhs.data_ == rhs.data_;
+    }
+
+    friend bool operator<(sequenced_map const & lhs, sequenced_map const & rhs)
+    {
+        return lhs.data_ < rhs.data_;
+    }
 };
 
 
 /*!
   挿入順のマルチマップ.
 
+  同一のキーが存在する場合は、必ず既存のデータの後にデータを追加する
   検索や終端でない位置に挿入する速度は、データ数に応じて線形的に遅くなるので、大きいデータを扱うのには不向き
  */
 template <
     typename Key,
     typename Value,
     typename Pred = std::equal_to<Key>,
-    typename Allocator = std::allocator< std::pair<Key const, Value> >
+    typename Alloc = std::allocator< std::pair<Key const, Value> >
     >
 class sequenced_multimap
-    : public detail::sequenced_map_base<detail::non_unique_tag, Key, Value, Pred, Allocator, std::deque>
+    : public acqua::container::detail::sequenced_map_base< sequenced_multimap<Key, Value, Pred, Alloc>, Key, Value, Pred, Alloc, std::deque>
+    , private boost::totally_ordered< sequenced_multimap<Key, Value, Pred, Alloc> >
 {
-    using base_type = detail::sequenced_map_base<detail::non_unique_tag, Key, Value, Pred, Allocator, std::deque>;
+private:
+    using base_type = typename sequenced_multimap::base_type;
+    using base_type::data_;
 
 public:
-    using key_type = typename base_type::key_type;
-    using mapped_type = typename base_type::mapped_type;
     using value_type = typename base_type::value_type;
-    using key_equal = typename base_type::key_equal;;
-    using allocator_type = typename base_type::allocator_type;
     using iterator = typename base_type::iterator;
     using const_iterator = typename base_type::const_iterator;
-    using reverse_iterator = typename base_type::reverse_iterator;
-    using const_reverse_iterator = typename base_type::const_reverse_iterator;
-    using difference_type = typename base_type::difference_type;
-    using size_type = typename base_type::size_type;
 
-    using base_type::base_type;
-    using base_type::key_eq;
-    using base_type::get_allocator;
-    using base_type::empty;
-    using base_type::size;
-    using base_type::clear;
     using base_type::begin;
     using base_type::end;
-    using base_type::rbegin;
-    using base_type::rend;
     using base_type::find;
-    using base_type::count;
-    using base_type::equal_range;
-    using base_type::insert;
-    using base_type::emplace;
-    using base_type::emplace_hint;
-    using base_type::erase;
-    using base_type::at;
-    using base_type::operator[];
+
+public:
+    sequenced_multimap(Pred const & key_eq = Pred(), Alloc const & alloc = Alloc())
+        : base_type(key_eq, alloc) {}
+
+    template <typename It>
+    explicit sequenced_multimap(It beg, It end)
+        : base_type(Pred(), Alloc())
+    {
+        base_type::insert(beg, end);
+    }
+
+    iterator insert(const_iterator, value_type const & val)
+    {
+        auto it = find(val.first);
+        if (it != end()) {
+            advance_to_next_key(it);
+            return data_.insert(it.base(), val);
+        }
+        data_.push_back(val);
+        return --end();
+    }
+
+    std::pair<iterator, bool> insert(value_type const & val)
+    {
+        return std::make_pair(insert(const_iterator(), val), true);
+    }
+
+    template <typename... Args>
+    iterator emplace_hint(const_iterator, Args... args)
+    {
+        auto it = find(args...);
+        if (it != end()) {
+            advance_to_next_key(it);
+            return data_.insert(it.base(), value_type(args...));
+        }
+        data_.emplace_back(args...);
+        return --end();
+    }
+
+    template <typename... Args>
+    std::pair<iterator, bool> emplace(Args... args)
+    {
+        return std::make_pair(emplace_hint(const_iterator(), args...), true);
+    }
+
+    friend bool operator==(sequenced_multimap const & lhs, sequenced_multimap const & rhs)
+    {
+        return lhs.data_ == rhs.data_;
+    }
+
+    friend bool operator<(sequenced_multimap const & lhs, sequenced_multimap const & rhs)
+    {
+        return lhs.data_ < rhs.data_;
+    }
+
+private:
+    template <typename It>
+    void advance_to_next_key(It & it) const
+    {
+        for(auto const & key = it->first; ++it != end();)
+            if (!Pred()(key, it->first))
+                break;
+    }
 };
 
 } }
